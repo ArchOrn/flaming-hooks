12b7fff057fb6a5a59f08ac8aadae59c4b3a856e
Raphael Kiffer <raphael@smartnsoft.com>
2015-04-07 16:09:06 +0200

    Add more logs and pull options.

--- app/config.js
+++ app/config.js
@@ -2,6 +2,10 @@ module.exports = {
   'github': {
     'urfmadness': {
       'repo': '/srv/www/archorn.eu/www/urfmadness',
+      'salt': 'd850049fksnm9c819d6ca13f04mfnsn0vdns209635jd039dndnd39cad5z9b2e7'
+    },
+    'webhooks': {
+      'repo': '/srv/www/archorn.eu/www/webhooks',
       'salt': 'ad5z9bd856359da392298ea36f482eb89c819e706c404cd6ca531fd39c13f209'
     }
   }
--- app/webhooks.js
+++ app/webhooks.js
@@ -8,6 +8,7 @@ var config = require(__base + 'app/config');
 exports.github = function (req, res) {
   var head = req.body.head_commit.id;
   var message = req.body.head_commit.message;
+  var repository = req.body.repository;
 
   console.log(req.body);
   console.log('Last commit: ' + head + ' has been pushed.');
@@ -18,12 +19,33 @@ exports.github = function (req, res) {
     console.log('Error: git is missing!');
   } else {
     var options = {};
-    // Go to repository
-    shell.cd(options, config.github.urfmadness.repo);
-    // Pull changes
-    shell.exec('git pull');
-    // Go back to previous folder
-    shell.cd(options, '-');
-    console.log('Done.');
+    var result = {
+      result: 'OK',
+      message: repository.full_name + 'is valid, processing...'
+    };
+    if (repository.full_name == 'ArchOrn/flaming-hooks') {
+      // Go to repository
+      shell.cd(options, config.github.webhooks.repo);
+      // Pull changes
+      shell.exec('git pull');
+      // Go back to previous folder
+      shell.cd(options, '-');
+      console.log('Done.');
+    } else if (repository.full_name == 'Jeekyx/angulurf') {
+      // Go to repository
+      shell.cd(options, config.github.urfmadness.repo);
+      // Pull changes
+      shell.exec('git pull');
+      // Go back to previous folder
+      shell.cd(options, '-');
+      console.log('Done.');
+    } else {
+      console.log('Error: repository not recognized!');
+      result = {
+        result: 'ERROR',
+        message: 'Error: repository not recognized!'
+      };
+    }
+    res.json(result);
   }
 };
