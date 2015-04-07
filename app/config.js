b2cd2d6d01d1a63aab56dedb7fb86f7dd82823a5
Raphael Kiffer <raphael@smartnsoft.com>
2015-04-07 15:53:12 +0200

    Fix config, missing colon.

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   'github': {
-    'urfmadness' {
+    'urfmadness': {
       'repo': '/srv/www/archorn.eu/www/urfmadness',
       'salt': 'ad5z9bd856359da392298ea36f482eb89c819e706c404cd6ca531fd39c13f209'
     }
